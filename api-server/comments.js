dfc7430dfc46d91bebdc63669d777ac30d038903
Calebe Arruda <calebearruda@gmail.com>
2018-07-12 22:18:31 -0300

    Update the posts and comments server files

--- api-server/comments.js
+++ api-server/comments.js
@@ -23,6 +23,46 @@ const defaultData = {
     voteScore: -5,
     deleted: false,
     parentDeleted: false
+  },
+  "894tuq4ut84ut8v4t8wun80o": {
+    id: '894tuq4ut84ut8v4t8wun80o',
+    parentId: "8xf0y6ziyjabvozdd22er",
+    timestamp: 1468166872634,
+    body: 'Nulla et suscipit velit. Ut eget feugiat odio. Cras ultricies ipsum lobortis, tempus lectus vel, sagittis lectus. Praesent scelerisque diam nulla, a elementum nisl imperdiet vitae. Morbi aliquet est nisi, sed auctor magna semper vel. Morbi tellus nunc, ultricies a posuere vitae, lacinia non mi. Proin tincidunt diam sit amet augue fringilla gravida. Integer eget lectus pellentesque, viverra tellus quis, mollis ex. Vestibulum a magna lobortis, ornare lacus quis, vehicula elit. Phasellus finibus urna ac mi viverra, et posuere erat gravida. Nullam in velit erat.',
+    author: 'thingtwo',
+    voteScore: 6,
+    deleted: false,
+    parentDeleted: false
+  },
+  "8tu4bsun805n8un48vw21": {
+    id: '8tu4bsun805n8un48vw21',
+    parentId: "8xf0y6ziyjabvozdd22er",
+    timestamp: 1469479767190,
+    body: 'Nullam enim risus, placerat ac tristique nec, semper lacinia erat. In ac accumsan lorem, non venenatis arcu. In vel dolor id nisi bibendum faucibus laoreet nec turpis. Donec non odio faucibus, posuere massa sed, posuere massa. Suspendisse convallis odio augue, in iaculis urna tristique eget. Maecenas scelerisque mauris nec ante venenatis tincidunt. Quisque in nibh mi. Proin mollis tortor gravida vulputate molestie.',
+    author: 'Yoda',
+    voteScore: 15,
+    deleted: false,
+    parentDeleted: false
+  },
+  "894tuq4ut84ut8v4t8wun82w": {
+    id: '894tuq4ut84ut8v4t8wun82w',
+    parentId: "8xf0y6ziyjabvozdd22df",
+    timestamp: 1468166872634,
+    body: 'Nulla et suscipit velit. Ut eget feugiat odio. Cras ultricies ipsum lobortis, tempus lectus vel, sagittis lectus. Praesent scelerisque diam nulla, a elementum nisl imperdiet vitae. Morbi aliquet est nisi, sed auctor magna semper vel. Morbi tellus nunc, ultricies a posuere vitae, lacinia non mi. Proin tincidunt diam sit amet augue fringilla gravida. Integer eget lectus pellentesque, viverra tellus quis, mollis ex. Vestibulum a magna lobortis, ornare lacus quis, vehicula elit. Phasellus finibus urna ac mi viverra, et posuere erat gravida. Nullam in velit erat.',
+    author: 'thingtwo',
+    voteScore: 1,
+    deleted: false,
+    parentDeleted: false
+  },
+  "8tu4bsun805n8un48vw24": {
+    id: '8tu4bsun805n8un48vw24',
+    parentId: "8xf0y6ziyjabvozdd22df",
+    timestamp: 1469479767190,
+    body: 'Nullam enim risus, placerat ac tristique nec, semper lacinia erat. In ac accumsan lorem, non venenatis arcu. In vel dolor id nisi bibendum faucibus laoreet nec turpis. Donec non odio faucibus, posuere massa sed, posuere massa. Suspendisse convallis odio augue, in iaculis urna tristique eget. Maecenas scelerisque mauris nec ante venenatis tincidunt. Quisque in nibh mi. Proin mollis tortor gravida vulputate molestie.',
+    author: 'Yoda',
+    voteScore: 2,
+    deleted: false,
+    parentDeleted: false
   }
 }
 
